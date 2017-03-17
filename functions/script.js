bceba5c89d04f07cdb0671327e6323cc8918739f
Alex <alex.formail@gmail.com>
2017-03-17 20:31:55 +0300

    add task folder

--- functions/script.js
+++ functions/script.js
@@ -4,7 +4,11 @@ console.log(plus(1, +"1"));
 console.log(plus(1, false));
 console.log(plus(1, true));
 console.log(div(10, 3));
-
+console.log(plus());//undefined->NaN
+console.log(plus(1));//undefined->NaN
+console.log(plus(null, null));//null->0
+console.log(plus("test ", "me"));
+console.log(plus("test ", "me", "again"));
 
 function plus(a, b) {
 	var result = a + b;
