9a7fb1924d59333bf952342ab991347723453bd0
Alex <alex.formail@gmail.com>
2017-03-17 19:38:48 +0300

    functions

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 console.log(plus(1, 1));
 console.log(plus(1, "1"));
-console.log(plus(1, true));
+console.log(plus(1, +"1"));
 console.log(plus(1, false));
+console.log(plus(1, true));
+console.log(div(10, 3));
 
 
 function plus(a, b) {
 	var result = a + b;
+	return result;
+}
+
+function minus(a, b) {
+	var result = a - b;
+	return result;
+}
+
+function multiply(a, b) {
+	var result = a * b;
+	return result;
+}
+
+function divide(a, b) {
+	var result = a * b;
+	return result;
+}
+
+function div(a, b) {
+	var result = (a - a % b) / b;
 	return result;
 }
