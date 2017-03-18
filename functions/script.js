8cb46d49c4af819657256b372acbaca5772c3a70
Alex <alex.formail@gmail.com>
2017-03-18 11:34:10 +0300

    new version of div(a, b) function

--- functions/script.js
+++ functions/script.js
@@ -9,6 +9,14 @@ console.log(plus(1));//undefined->NaN
 console.log(plus(null, null));//null->0
 console.log(plus("test ", "me"));
 console.log(plus("test ", "me", "again"));
+//new tests
+console.log(div(3.5, 2));
+console.log(divT(3.5, 2));
+console.log(div(-1.5, 2));
+console.log(divT(-1.5, 2));
+console.log(div(5, 3));
+console.log(divT(5, 3));
+
 
 function plus(a, b) {
   return a + b;
@@ -29,3 +37,17 @@ function divide(a, b) {
 function div(a, b) {
   return (a - a % b) / b;
 }
+
+function divT(a, b) {
+  return trunc(a/b);;
+}
+
+function trunc(x) {
+  if (isNaN(x)) {
+    return NaN;
+  }
+  if (x > 0) {
+    return Math.floor(x);
+  }
+  return Math.ceil(x);
+};
