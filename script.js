d28bd6714cfc40b2723b5144243d90d51233ee4e
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2017-11-22 16:41:08 +0300

    semantic marcut is added

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ const url = 'https://newsapi.org/v2/top-headlines?' +
 
 const req = new Request(url);
 
+const htmlToElement = html => {
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
 fetch(req)
-  .then(response => response.json())
-  .then(({ articles }) => console.log(articles))
-  .catch(error => console.log(error.message));
+  .then(
+    response => response.json()
+  )
+  .then(
+    ({ articles }) => {
+      console.log(articles);
+      document.querySelector("main").appendChild(htmlToElement(articles[1].author));
+    }
+  )
+  .catch(
+    error => console.log(error.message)
+  );
+
+
+document.main.appendChild(htmlToElement());
